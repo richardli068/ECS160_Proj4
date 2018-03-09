8d1f942f364d2cf769f1b71a9ffc06015ece3ace
richardli <srli@ucdavis.edu>
2018-03-08 21:02:41 -0800

    Added funcitons

--- maxTweeter.c
+++ maxTweeter.c
@@ -6,9 +6,35 @@
 //
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(const char* filePath);
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  // no command line input
+  if (argc != 3)
+  {
+    printf("No Input\n");
+    return 0;
+  }
+  
+  const char* filePath = argv[2];
+  
+  // invalid csv
+  if(!isValid(filePath))
+  {
+    printf("Invalid Input Format\n");
+    return 0;
+  }
+  
+  // valid csv
+  // TODO
+  
   return 0;
+}
+
+bool isValid(const char* filePath)
+{
+  return false;
 }
