8e5b558e04782c60e991dd54d4e9deea32eba23f
richardli <srli@ucdavis.edu>
2018-03-08 22:08:21 -0800

    Worked on isValid(). Added test file.

--- maxTweeter.c
+++ maxTweeter.c
@@ -6,20 +6,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 377
+#define LINE_LENGTH 400
+
 bool isValid(const char* filePath);
+void printInvalid();
+
 
 int main(int argc, const char * argv[]) {
   // no command line input
-  if (argc != 3)
+  if (argc != 2)
   {
     printf("No Input\n");
     return 0;
   }
   
-  const char* filePath = argv[2];
+  const char* filePath = argv[1];
   
   // invalid csv
   if(!isValid(filePath))
@@ -36,5 +42,34 @@ int main(int argc, const char * argv[]) {
 
 bool isValid(const char* filePath)
 {
-  return false;
+  FILE *fp = fopen(filePath, "r");
+  char line[LINE_LENGTH];
+  
+  // Error opening file
+  if (!fp)
+  {
+    printInvalid();
+    return false;
+  }
+  
+  // read line
+  fscanf(fp, "%s", line);
+  
+  // line too long
+  if (strlen(line) > MAX_LINE_LENGTH)
+  {
+    printInvalid();
+    return false;
+  }
+  
+  // TODO
+  // split by ',', if not every token starts and ends with ", invalid.
+  
+  fclose(fp);
+  return true;
+}
+
+void printInvalid()
+{
+  printf("Invalid Input Format\n");
 }
