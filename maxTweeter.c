c144b5a4fdff726dfe63b94c1141d0486d6a0671
richardli <srli@ucdavis.edu>
2018-03-08 20:02:20 -0800

    Initial commit

---
+++ maxTweeter.c
@@ -0,0 +1,14 @@
+//
+//  maxTweeter.c
+//  maxTweeter
+//
+//  Created by Richard Li on 3/8/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
